1ce10dbdcf13f0d16b755089af1fa80eff19dee6
zzxian <80020410+sduzzx857@users.noreply.github.com>
2024-12-04 14:41:22 +0800

    Update demo_2x.py

--- demos/demo_2x.py
+++ demos/demo_2x.py
@@ -8,17 +8,54 @@ import cv2
 from omegaconf import OmegaConf
 import tifffile as tf
 import shutil
+import argparse
+import xml.etree.ElementTree as ET
 sys.path.append('.')
 from utils.build_utils import build_from_cfg
 from utils.utils import read, img2tensor
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+def extract_particle_location(args):
+    save = args.loc_path
+    xml_path = os.path.join(args.img_path, '_Tracks.xml')
+    if not os.path.exists(save):
+        os.makedirs(save)
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
 
-def output(model):
-    strPath = 'demos/images'
-    savePath = 'demos/results'
-    pcPath = 'demos/particle-loc'
+    pos = []
+
+    for particle in root.findall('.//particle'):
+        for i, detection1 in enumerate(particle.findall('detection')):
+            if i < len(particle.findall('detection')): 
+                t1 = int(detection1.get('t'))
+                x1 = float(detection1.get('x'))
+                y1 = float(detection1.get('y'))       
+                if t1 >= len(pos):
+                    for j in range(t1 - len(pos) + 1):
+                        pos.append([])
+                pos[t1].append([x1, y1])
+
+    print("frame num:", len(pos))
+    max_num = 0
+    for i in range(len(pos)):
+        p = np.array(pos[i])
+
+        # Sort vertices by y then x
+        sort_inds = np.lexsort(p.T)
+        p = p[sort_inds]
+
+        file = 't%04d.npz'%(i)
+        np.savez(os.path.join(save, file), pos=p)
+        max_num = max(p.shape[0], max_num)
+    
+    print("The peak number of particles: ", max_num)
+
+def output(model, args):
+    strPath = args.img_path
+    savePath = args.save_path
+    pcPath = args.loc_path
 
     if not osp.exists(savePath):
         os.makedirs(savePath)   
@@ -63,9 +100,19 @@ def output(model):
         tf.imwrite(osp.join(savePath, 'image%04d.tif' % (2*i+1)), I1)      
 
 
-cfg = 'EB1.yaml'
+parser = argparse.ArgumentParser()
+parser.add_argument('--cfg', default='EB1.yaml') 
+parser.add_argument('--ckpt', default='EB1.pth') 
+parser.add_argument('--img_path', default='demos/images') 
+parser.add_argument('--loc_path', default='demos/particle-loc') 
+parser.add_argument('--save_path', default='demos/results') 
+args = parser.parse_args()
+
+extract_particle_location(args)
+
+cfg = args.cfg
 cfg_path = os.path.join('cfgs',cfg)
-ckpt = 'EB1.pth'
+ckpt = args.ckpt
 ckpt_path = os.path.join('pretrained/', ckpt)
 network_cfg = OmegaConf.load(cfg_path).network
 network_cfg.params.gmsf_pretrained=''
@@ -82,5 +129,5 @@ model.load_state_dict(ckpt['state_dict'])
 model = model.to(device)
 model.eval()
 
-output(model)
+output(model, args)
 
